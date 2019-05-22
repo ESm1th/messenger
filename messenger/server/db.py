5943c3187923cd188f5ffa79deef7e173df9cfc1
Evgeny <ku7nec0v@gmail.com>
2019-05-22 22:28:37 +0300

    [server] Annotations added to db.py

--- messenger/server/db.py
+++ messenger/server/db.py
@@ -20,7 +20,7 @@ from sqlalchemy import (
     func
 )
 from contextlib import AbstractContextManager
-from typing import List
+from typing import List, Union
 
 
 engine = create_engine('sqlite:///db.sqlite', echo=True)
@@ -60,26 +60,26 @@ class CoreMixin:
             return session.query(cls).all()
 
     @classmethod
-    def create(cls, seance, *args, **kwargs):
+    def create(cls, seance, *args, **kwargs) -> None:
         with SessionScope(seance) as session:
             obj = cls(*args, **kwargs)
             session.add(obj)
             session.commit()
 
-    def update(self, seance, *args, **kwargs):
+    def update(self, seance, *args, **kwargs) -> None:
         with SessionScope(seance) as session:
             for key, value in kwargs.items():
                 setattr(self, key, value)
             session.add(self)
             session.commit()
 
-    def delete(self, seance):
+    def delete(self, seance) -> None:
         with SessionScope(seance) as session:
             session.delete(self)
             session.commit()
 
 
-class User(CoreMixin, Base):
+class User(CoreMixin, Base):  # type: ignore
     """Represents 'users' table in database"""
 
     first_name = Column(String)
@@ -101,7 +101,7 @@ class User(CoreMixin, Base):
     chats = relationship('Chat', back_populates='creator')
     avatar = relationship('Media', back_populates='user')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<User(first_name={0}, second_name={1}, username={2})>'.format(
             self.first_name,
             self.second_name,
@@ -109,13 +109,13 @@ class User(CoreMixin, Base):
         )
 
     @classmethod
-    def get_user(cls, session, username):
+    def get_user(cls, session, username) -> Union['User', None]:
         with SessionScope(session) as session:
             return session.query(cls).filter(
                 cls.username == username).one_or_none()
 
 
-class Chat(CoreMixin, Base):
+class Chat(CoreMixin, Base):  # type: ignore
     """Represents 'chats' table in database"""
 
     title = Column(String)
@@ -132,7 +132,7 @@ class Chat(CoreMixin, Base):
     )
     picture = relationship('Media', back_populates='chat')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<Chat(id={0}, type={1}, creator={2})>'.format(
             self.id,
             self.type,
@@ -140,7 +140,7 @@ class Chat(CoreMixin, Base):
         )
 
 
-class Message(CoreMixin, Base):
+class Message(CoreMixin, Base):  # type: ignore
     """Represents 'messages' table in database"""
 
     text = Column(Text)
@@ -153,7 +153,7 @@ class Message(CoreMixin, Base):
     chat = relationship('Chat', back_populates='messages')
     media = relationship('Media', back_populates='message')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<Message(id={0}, user={1}, chat={2})>'.format(
             self.id,
             self.user,
@@ -161,7 +161,7 @@ class Message(CoreMixin, Base):
         )
 
 
-class Media(CoreMixin, Base):
+class Media(CoreMixin, Base):  # type: ignore
     """Represents 'media' table in database"""
 
     __tablename__ = 'media'
@@ -173,7 +173,7 @@ class Media(CoreMixin, Base):
     user = relationship('User', uselist=False, back_populates='avatar')
     chat = relationship('Chat', uselist=False, back_populates='picture')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<Media(id={0}, path={1}, message={2})>'.format(
             self.id,
             self.path,
